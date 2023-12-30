9e7268162773eb68eb2070ee9f6dbce649394966
westcode6 <westcode6@gmail.com>
2023-12-30 01:03:36 +0100

    new changes everyheres today3

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from "@vitejs/plugin-react";
 import beep from "@rollup/plugin-beep"
 export default defineConfig({
   plugins: [react(), beep()],
-  onwarn(warning, warn)
-  {
-      if (warning.code == 'EVAL' && /[\\/]node_modules[\\/]firebase[\\/]/.test(warning.id)) return;
-
-      warn(warning);
-  },
+ 
 
   build: {
     chunkSizeWarningLimit: 100000000,
