846695d2bcdde5ec65a0b34c28cbf136d54bb340
westcode6 <westcode6@gmail.com>
2023-12-30 10:40:18 +0100

    Spirit has taken over

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import beep from "@rollup/plugin-beep"
-export default defineConfig({
-  plugins: [react(), beep()],
- 
+import beep from "@rollup/plugin-beep";
 
-  build: {
-    chunkSizeWarningLimit: 100000000,
-  },
-})
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // vite config
+    plugins: [react(), beep()],
+    onwarn(warning, warn) {
+      if (
+        warning.code == "EVAL" &&
+        /[\\/]node_modules[\\/]firebase[\\/]/.test(warning.id)
+      )
+        return;
+
+      warn(warning);
+    },
+
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
+});
